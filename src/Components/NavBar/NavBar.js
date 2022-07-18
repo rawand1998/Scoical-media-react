279d19ba0c91632c25aeedf284405a29fa612ade
rawand1998 <rawandgaradh@gmail.com>
2022-07-18 15:48:30 +0300

    navbar style

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Style.css'
 function NavBar() {
   return (
-    <div>
-        <Link to='/login'>Login</Link>
-        
+    <div className="nav-container">
+      <h1>Soical Media</h1>
+      <div className="nav">
+        <Link to='/login' className="list">Login</Link>
+        <Link to='/profile' className="list">Profile</Link>
+        </div>
     </div>
   )
 }
