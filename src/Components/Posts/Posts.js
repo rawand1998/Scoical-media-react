8a66fd09fc30b00d5777246b02c375c4c9a582d6
rawand1998 <rawandgaradh@gmail.com>
2022-07-19 08:49:18 +0300

    posts style

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -17,12 +17,18 @@ function Posts() {
   return (
     <div>
         <div>
-       get all posts 
+    
       {allPost.map((item)=>
       
-      <div>
-        <p key={item.id}>{item.text}</p>
-        <p>{item.name}</p>
+      <div className="dispaly-post-container">
+        <div>
+          <div className="post-data">
+            <img src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/>
+        <p className="post-userName">{item.name}</p>
+        </div>
+        <p key={item.id }className="post-text">{item.text}</p>
+       
+        </div>
         <div>
             <CommentInput postId={item.id}/>
             <button onClick={showComment}>show comments
